a4b75184c9f0684219dbff88c0d2cf127e5ceb6d
MindbodySupriya <supriya.nazirkar@mindbodyonline.com>
2019-07-03 15:15:46 +0530

    Added Automated Tests

--- app/src/androidTest/java/Model/ModelClass.kt
+++ app/src/androidTest/java/Model/ModelClass.kt
@@ -7,6 +7,24 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.myapplication.R
 
 class ModelClass {
+    private val text = onView(withId(R.id.textData)) //locators
+    private val showAnotherFactButton = onView(withId(R.id.showFactButton))
+
+    fun verifyTextIsNull() {
+        if (text != null) {
+            println("TextView is not null")
+        } else
+            println("TextView is null")
+    }
+
+    fun verifyText() {
+        text.check(ViewAssertions.matches(withText("Did you know?")))
+    }
+
+    fun clickShowAnotherButton() {
+        showAnotherFactButton.perform(click())
+            .check(ViewAssertions.matches(isClickable()))
+    }
 
 
 }
--- app/src/androidTest/java/Tests/TestsClass.kt
+++ app/src/androidTest/java/Tests/TestsClass.kt
@@ -8,5 +8,23 @@ import org.junit.Test
 
 class TestsClass {
 
-    
+    private val modelFun = ModelClass()
+
+    @get:Rule
+    var mActivityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun verifyTextIsNull(){
+        modelFun.verifyTextIsNull()
+    }
+
+    @Test
+    fun verifyText(){
+        modelFun.verifyText()
+    }
+
+    @Test
+    fun clickButton() {
+        modelFun.clickShowAnotherButton()
+    }
 }
